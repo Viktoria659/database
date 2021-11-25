0a87b3547bbdc1d6c3351d4337221cca0d2ece19
Viktoria <catvicaf@yandex.ru>
2021-11-25 05:09:22 +0300

    Added schema auth (account)

--- psql/V1__init.sql
+++ psql/V1__init.sql
@@ -36,3 +36,29 @@ Alter table auth.usr
 
 ALTER TABLE auth.usr
     OWNER to ${auth_user};
+
+-- Schema: client
+CREATE SCHEMA IF NOT EXISTS client AUTHORIZATION ${client_user};
+
+-- Table: client.account
+CREATE TABLE IF NOT EXISTS client.account
+(
+    client_id  BIGINT NOT NULL UNIQUE,
+    user_id    BIGINT NOT NULL,
+    firstname  TEXT   NOT NULL,
+    surname    TEXT   NOT NULL,
+    patronymic TEXT,
+    birthday   DATE,
+    PRIMARY KEY (client_id),
+    CONSTRAINT user_id FOREIGN KEY (user_id)
+    REFERENCES auth.usr (user_id) MATCH SIMPLE
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+    );
+
+Create sequence IF not exists client.account_id_seq START 1;
+Alter table client.account
+    Alter column client_id set default nextval('client.account_id_seq');
+
+ALTER TABLE client.account
+    OWNER to ${bank_user};
