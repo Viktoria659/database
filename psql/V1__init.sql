37f60990a3f1d3cbff04eef3fa08b670e96d0dd3
Viktoria <catvicaf@yandex.ru>
2022-03-07 17:09:40 +0300

    Add new table and fixed old

--- psql/V1__init.sql
+++ psql/V1__init.sql
@@ -1,64 +1,107 @@
--- Schema: auth
-CREATE SCHEMA IF NOT EXISTS auth AUTHORIZATION ${auth_user};
+CREATE SCHEMA IF NOT EXISTS bank AUTHORIZATION ${bank_user};
 
--- Table: auth.role
-CREATE TABLE IF NOT EXISTS auth.roles
+-- Table: bank.role
+CREATE TABLE IF NOT EXISTS bank.role
 (
     role_id BIGINT NOT NULL UNIQUE,
     role    TEXT   NOT NULL UNIQUE,
     primary key (role_id)
     );
 
-INSERT INTO auth.roles
+INSERT INTO bank.role
 VALUES ('1', 'ROLE_USER');
+INSERT INTO bank.role
+VALUES ('2', 'ROLE_ADMIN');
 
-Create sequence IF not exists auth.roles_id_seq START 2;
-Alter table auth.roles
-    Alter column role_id set default nextval('auth.roles_id_seq');
+Create sequence if not exists bank.role_id_seq START 2;
+Alter table bank.role
+    Alter column role_id set default nextval('bank.role_id_seq');
 
-ALTER TABLE auth.roles
-    OWNER to ${auth_user};
+ALTER TABLE bank.role
+    OWNER to ${bank_user};
 
--- Table: auth.usr
-CREATE TABLE IF NOT EXISTS auth.usr
-(
-    user_id  BIGINT NOT NULL UNIQUE,
-    username TEXT   NOT NULL UNIQUE,
-    password TEXT   NOT NULL,
-    active   BOOLEAN         DEFAULT true,
-    role     BIGINT NOT NULL DEFAULT 1 REFERENCES auth.roles (role_id),
-    PRIMARY KEY (user_id)
-);
-
-Create sequence IF not exists auth.usr_id_seq START 1;
-Alter table auth.usr
-    Alter column user_id set default nextval('auth.usr_id_seq');
-
-ALTER TABLE auth.usr
-    OWNER to ${auth_user};
-
--- Schema: client
-CREATE SCHEMA IF NOT EXISTS client AUTHORIZATION ${client_user};
-
--- Table: client.client
-CREATE TABLE IF NOT EXISTS client.client
+-- Table: bank.client
+CREATE TABLE IF NOT EXISTS bank.client
 (
     client_id  BIGINT NOT NULL UNIQUE,
-    user_id    BIGINT NOT NULL,
     firstname  TEXT   NOT NULL,
     surname    TEXT   NOT NULL,
     patronymic TEXT,
     birthday   DATE,
-    PRIMARY KEY (client_id),
-    CONSTRAINT user_id FOREIGN KEY (user_id)
-    REFERENCES auth.usr (user_id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
+    PRIMARY KEY (client_id)
+);
+
+CREATE SEQUENCE IF NOT EXISTS bank.client_id_seq START 1;
+Alter table bank.client
+    Alter column client_id set default nextval('bank.client_id_seq');
+
+ALTER TABLE bank.client
+    OWNER to ${bank_user};
+
+-- Table: bank.usr
+CREATE TABLE IF NOT EXISTS bank.usr
+(
+    user_id  BIGINT NOT NULL
+        CONSTRAINT usr_pkey
+            PRIMARY KEY
+        CONSTRAINT fko1354jy5tci8vpw2gb4x3tbeo
+            REFERENCES client,
+    username TEXT   NOT NULL UNIQUE,
+    password TEXT   NOT NULL,
+    active   BOOLEAN         DEFAULT true,
+    role     BIGINT NOT NULL DEFAULT 1 REFERENCES bank.role (role_id)
+);
+
+ALTER TABLE bank.usr
+    OWNER to ${bank_user};
+
+CREATE TABLE IF NOT EXISTS bank.account
+(
+    account_id    BIGINT NOT NULL UNIQUE,
+    balance       BIGINT,
+    version       BIGINT,
+    created_date  TIMESTAMP,
+    modified_date TIMESTAMP,
+    comment       TEXT,
+    client_id     BIGINT
+        CONSTRAINT fkkm8yb63h4ownvnlrbwnadntyn
+            REFERENCES client,
+    PRIMARY KEY (account_id)
+);
+
+CREATE SEQUENCE IF NOT EXISTS bank.account_id_seq START 1;
+ALTER TABLE bank.account
+    ALTER COLUMN account_id SET DEFAULT nextval('bank.account_id_seq');
+
+ALTER TABLE bank.account
+    OWNER TO ${bank_user};
+
+CREATE TABLE IF NOT EXISTS bank.revinfo
+(
+    rev      INTEGER NOT NULL
+        CONSTRAINT revinfo_pkey
+            PRIMARY KEY,
+    revtstmp BIGINT
+);
+
+ALTER TABLE bank.revinfo
+    OWNER TO ${bank_user};
+
+
+CREATE TABLE IF NOT EXISTS bank.account_aud
+(
+    account_id    BIGINT  NOT NULL UNIQUE,
+    rev           INTEGER NOT NULL
+        CONSTRAINT fkaexie5n0kol2mjlvo03ii45d0
+            REFERENCES revinfo,
+    revtype       SMALLINT,
+    balance       BIGINT,
+    modified_date TIMESTAMP,
+    created_date  TIMESTAMP,
+    comment       TEXT,
+        CONSTRAINT account_aud_pkey
+            PRIMARY KEY (account_id, rev)
     );
 
-Create sequence IF not exists client.client_id_seq START 1;
-Alter table client.client
-    Alter column client_id set default nextval('client.client_id_seq');
-
-ALTER TABLE client.client
-    OWNER to ${bank_user};
+ALTER TABLE bank.account_aud
+    OWNER TO ${bank_user};
